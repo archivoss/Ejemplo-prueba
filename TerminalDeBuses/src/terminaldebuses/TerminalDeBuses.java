67af11a3fca3ed0a9c1bfcb972d34a3ed1af62cc
Francisco <quevedof98@gmail.com>
2020-05-21 22:01:46 -0400

    reset

--- TerminalDeBuses/src/terminaldebuses/TerminalDeBuses.java
+++ TerminalDeBuses/src/terminaldebuses/TerminalDeBuses.java
@@ -22,7 +22,7 @@ public class TerminalDeBuses extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        //a
         stage.setScene(scene);
         stage.show();
     }
